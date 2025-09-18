10a6dbcda8a1cfc0f676b3e0a09e96d6c4c367d8
jairocomino <jairocominoruiz@gmail.com>
2025-09-18 17:22:39 +0200

    Tu mensaje de commit

--- Practicas/React/Proyectos/06-proyecto1/src/components/Crear.js
+++ Practicas/React/Proyectos/06-proyecto1/src/components/Crear.js
@@ -1,14 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { guardarEnEstorage } from '../helpers/guardarEnEstoraje';
 
 const Crear = () => {
+  const tituloComp = "Añadir Pelicula";
+
+   const [pelicula, setPelicula] = useState({
+    titulo: "",
+    descripcion: ""
+  });
+ const {titulo, description} = pelicula;
+
+  const conseguirDatosForm = e => {
+    e.preventDefault();
+    console.log("Formulario enviado");
+    // Recoger los datos del formulario
+    let pelicula = e.target;
+    let titulo = pelicula.titulo.value;
+    let description = pelicula.description.value;
+
+    // Crear objeto de la película
+    let peliculaObj = {
+      id: new Date().getTime(),
+      titulo,
+      description
+    };
+    // Actualizar el estado
+    setPelicula(peliculaObj);
+    // Guardar en el localstorage
+    guardarEnEstorage(peliculaObj,"pelis");
+    // Resetear el formulario
+    e.target.reset();
+   
+  }
+
+  // const guardarEnEstorage=pelicula=>{
+
+  //   //conseguir elementos
+  //   let items = JSON.parse(localStorage.getItem("pelis"));
+  //   //comprovar si esd un array
+  //   if(Array.isArray(items)){
+  //     //añadir la nueva pelicula
+  //     items.push(pelicula);
+  //   }else{
+  //     //crear array y añadir la pelicula
+  //     items = [pelicula];
+  //   }
+  //   //guardar   el array en el localstorage
+  //   localStorage.setItem("pelis", JSON.stringify(items));
+
+  //      return  pelicula
+  // }
+
   return (
    <>
     <div className="add">
-            <h3 className="title">Añadir Pelicula</h3>
-            <form action="#" method="post">
-              <input type="text" placeholder="Título de la película" name="title" required />
-              <textarea placeholder="Descripción" name="description"></textarea>
-              <button type="submit">Añadir</button>
+            <h3 className="title">{tituloComp}</h3>
+            <strong>
+               {(titulo && description) && "Has creado la película: " + titulo}
+            </strong>
+            <form action="#"  onSubmit={conseguirDatosForm}>
+              
+              <input type="text" 
+                placeholder="Título de la película" 
+                id='titulo'
+                name='titulo'
+           
+                required />
+
+              <textarea  
+                id='description'
+                placeholder="Descripción" 
+                name='description'
+              
+                required
+                > 
+
+                </textarea>
+
+              <button 
+                type="submit"
+                id='save'>
+                Añadir
+              </button>
+
             </form>
           </div>
    </>
---
+++ Practicas/React/Proyectos/06-proyecto1/src/helpers/guardarEnEstoraje.js
@@ -0,0 +1,16 @@
+  export const guardarEnEstorage=(item,clave)=>{
+
+    //conseguir elementos
+    let items = JSON.parse(localStorage.getItem(clave));
+    //comprovar si esd un array
+    if(Array.isArray(items)){
+      //añadir el nuvo elemento
+      items.push(item);
+    }else{
+      //crear array y añadir el item
+      items = [item];
+    }
+    //guardar   el array en el localstorage
+    localStorage.setItem(clave, JSON.stringify(items));
+       return  item
+  }
